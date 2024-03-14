52d6a5b31287fe832057e8492d4ad6d7f939e44d
NA-E <eka.nourin@gmail.com>
2024-03-13 21:37:27 -0400

    Update LoginHero.jsx

--- src/components/LoginHero.jsx
+++ src/components/LoginHero.jsx
@@ -11,11 +11,11 @@ function Hero({handleLogIn}) {
         <Card>
           <FlexColumnCenter>
             <h1>Open Sauced Dashboard</h1>
-            <p>Login with GitHub to start tracking open source projects.</p>
+            <p>Login with GitHub to start tracking open-source projects.</p>
             <br />
             <FlexCenter>
             <MarketingButton alt="pizza" primary onClick={handleLogIn}>
-              Login with GitHub
+              You can Login with GitHub
             </MarketingButton>
           </FlexCenter>
           </FlexColumnCenter>
